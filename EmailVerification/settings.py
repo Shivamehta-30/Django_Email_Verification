6dd0db9883593e0e6343784f0ede1de99837a677
MEHTA SHIVAM <84615426+Shivamehta-30@users.noreply.github.com>
2021-08-18 18:30:40 +0530

    Update settings.py

--- EmailVerification/settings.py
+++ EmailVerification/settings.py
@@ -130,5 +130,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shivammehta.180670107038@gmail.com'
-EMAIL_HOST_PASSWORD = '180670107038'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'email password'
